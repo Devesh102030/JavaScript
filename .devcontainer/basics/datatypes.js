a88e7c35cfbb86f37a29dd2c9c960e5355e130db
Devesh102030 <127661847+Devesh102030@users.noreply.github.com>
2023-12-30 15:10:56 +0000

    variables  and datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //use newer version of js
+
+//alert(3+3) cannot be used because we are using nodejs not browser//
+
+let name = "Devesh"
+let age = 18
+let isloggedin = false
+
+//number -> 2^53
+//bigint
+//string -> ""
+//boolean -> true/false
+//null -> standalone value
+//undefined -> no value assinged
+// symbol -> unique
+//object
+
+console.log(typeof "devesh"); //gives datatype
+console.log(typeof null); //gives output object
+console.log(typeof undefined); //gives output undefined
---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,14 @@
+const accountID = 1245
+let accountemail = "hello@gmail.com"
+var accountPassword = "world123"
+accountCity = "jaipur"
+let accountState;
+var accountDistrict;
+
+accountemail = "yo@yahoo.com"
+
+/*prefer not to use var
+ because let is block scope and var is functional scope */
+
+console.log(accountID)
+console.table([accountID,accountemail,accountPassword,accountCity,accountState,accountDistrict])
